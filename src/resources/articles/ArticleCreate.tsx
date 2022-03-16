66503028d3367516d460fe802b4d340c0fe652e9
Simon 徐 <simonxu@SimondeMacBook-Pro.local>
2022-03-16 08:53:48 +0800

    mm

--- src/resources/articles/ArticleCreate.tsx
+++ src/resources/articles/ArticleCreate.tsx
@@ -1,11 +1,12 @@
 import { Create, SimpleForm, TextInput, CreateProps } from "react-admin";
+import RichEditor from "../../inputs/RichEditor";
 
 export const ArticleCreate = (props: CreateProps): JSX.Element => (
   <Create {...props}>
     <SimpleForm>
       <TextInput source="title" />
       <TextInput source="cover" />
-      <TextInput source="body" />
+      <RichEditor source="body" />
     </SimpleForm>
   </Create>
 );
